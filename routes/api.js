780f6fe1b87ed419f8cc9849aabacdf3fa8a2024
Ian Kahwai <iankahwai@gmail.com>
2019-02-08 18:01:33 +0200

    Add shorturl/new endpoint to handle website link addresses posted

--- routes/api.js
+++ routes/api.js
@@ -7,4 +7,8 @@ router.get("/hello", function (req, res) {
     res.json({greeting: 'hello API'});
 });
 
+router.post('/shorturl/new', (req, res)=>{
+   
+    res.end();
+});
 module.exports = router;
