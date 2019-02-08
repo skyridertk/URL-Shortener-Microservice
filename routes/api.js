a57e5adcc73009f889ea433805cd2e5848ac8a1c
Ian Kahwai <iankahwai@gmail.com>
2019-02-08 17:57:16 +0200

    Create routes dir to store route handlers

---
+++ routes/api.js
@@ -0,0 +1,10 @@
+'use-strict'
+
+const express = require('express');
+const router = express.Router();
+
+router.get("/hello", function (req, res) {
+    res.json({greeting: 'hello API'});
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -27,9 +27,7 @@ app.get('/', function(req, res){
 
   
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+app.use('/api', require('./routes/api'));
 
 
 app.listen(port, function () {
